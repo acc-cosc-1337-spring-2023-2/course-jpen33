0f082dd679bbdd04b50ebfa5764dc865b1b5b71b
jpen33 <123610676+jpen33@users.noreply.github.com>
2023-04-08 21:04:36 -0500

    Update tic_tac_toe_manager.cpp

--- src/homework/06_tic_tac_toe/tic_tac_toe_manager.cpp
+++ src/homework/06_tic_tac_toe/tic_tac_toe_manager.cpp
@@ -1,1 +1,47 @@
 //cpp
+#include "tic_tac_toe_manager.h"
+#include "tic_tac_toe.h"
+
+void TicTacToeManager::update_winner_count(string winner)
+{
+    if(winner == "X")
+    {
+        x_win++;
+    }
+    
+  else if(winner == "O")
+    {
+        o_win++;
+    }
+    
+  else
+    {
+        ties++;
+    }
+}
+
+void TicTacToeManager::save_game(TicTacToe b)
+{
+   
+    update_winner_count(b.get_winner());
+}
+
+std::ostream& operator<<(std::ostream& out, const TicTacToeManager& manager)
+{
+ 
+    for ( const auto& game : manager.games)
+    {
+        out << game << "\n";
+    }
+    return out;
+}
+
+void TicTacToeManager::get_winner_total(int& o, int& x, int& t)
+{
+    o = o_win;
+    x = x_win;
+    t = ties;
+}
+
+{
+}
