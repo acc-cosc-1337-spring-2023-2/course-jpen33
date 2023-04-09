79c6c603f64a7f5ff64721734a1ddb863a3744b9
jpen33 <123610676+jpen33@users.noreply.github.com>
2023-04-08 21:03:27 -0500

    Update tic_tac_toe_tests.cpp

--- test/homework/06_tic_tac_toe/tic_tac_toe_tests.cpp
+++ test/homework/06_tic_tac_toe/tic_tac_toe_tests.cpp
@@ -157,3 +157,77 @@ TEST_CASE("Test Win diagonlly from the bottom left")
 	test_instance.mark_board(3);
 	REQUIRE(test_instance.game_over() == true);
 }
+
+TEST_CASE("Verify that x wins, o wins, and ties works")
+{
+	TicTacToe test_instance;
+	TicTacToeManager manager_instance;
+
+	test_instance.start_game("X");
+
+	test_instance.mark_board(7);
+	test_instance.mark_board(1);
+	test_instance.mark_board(5);
+	test_instance.mark_board(2);
+	test_instance.mark_board(3);
+	REQUIRE(test_instance.game_over() == true);
+	REQUIRE(test_instance.get_winner() == "X");
+
+	manager_instance.save_game(test_instance);
+	int o, x, t;
+    manager_instance.get_winner_total(o, x, t);
+    REQUIRE(o == 0);
+    REQUIRE(x == 1);
+    REQUIRE(t == 0);
+	
+	
+	test_instance.start_game("O");
+
+	test_instance.mark_board(1);
+	test_instance.mark_board(2);
+	test_instance.mark_board(5);
+	test_instance.mark_board(3);
+	test_instance.mark_board(9);
+	REQUIRE(test_instance.game_over() == true);
+	REQUIRE(test_instance.get_winner() == "O");
+        
+	manager_instance.save_game(test_instance);
+	
+	
+
+    manager_instance.get_winner_total(o, x, t);
+    REQUIRE(o == 1);
+    REQUIRE(x == 1);
+    REQUIRE(t == 0);
+
+	
+	test_instance.start_game("X");
+
+	test_instance.mark_board(1);
+	REQUIRE(test_instance.game_over() == false);
+	test_instance.mark_board(2);
+	REQUIRE(test_instance.game_over() == false);
+	test_instance.mark_board(3);
+	REQUIRE(test_instance.game_over() == false);
+	test_instance.mark_board(4);
+	REQUIRE(test_instance.game_over() == false);
+	test_instance.mark_board(5);
+	REQUIRE(test_instance.game_over() == false);
+	test_instance.mark_board(7);
+	REQUIRE(test_instance.game_over() == false);
+	test_instance.mark_board(6);
+	REQUIRE(test_instance.game_over() == false);
+	test_instance.mark_board(9);
+	REQUIRE(test_instance.game_over() == false);
+	test_instance.mark_board(8);
+	REQUIRE(test_instance.game_over() == true);
+	REQUIRE(test_instance.get_winner() == "C");
+
+    
+	
+    manager_instance.save_game(test_instance);
+	
+    manager_instance.get_winner_total(o, x, t);
+    REQUIRE(o == 1);
+    REQUIRE(x == 1);
+    REQUIRE(t == 1);
