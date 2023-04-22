bc86bba50fe38525152a95b930c99d79b59c53cc
jpen33 <123610676+jpen33@users.noreply.github.com>
2023-04-22 15:45:32 -0500

    Update tic_tac_toe_4.h

--- src/homework/06_tic_tac_toe/tic_tac_toe_4.h
+++ src/homework/06_tic_tac_toe/tic_tac_toe_4.h
@@ -10,10 +10,13 @@ class TicTacToe4 : public TicTacToe
 public:
     TicTacToe4() : TicTacToe(4){}
     
+    TicTacToe3(vector<string> p, string winner) : TicTacToe(p, winner){}
 private:
     bool check_column_win() override;
     bool check_row_win() override;
     bool check_diagonal_win() override;
 };
+
+
 
 #endif
